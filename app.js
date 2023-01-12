9a89beb76eec6f8cdd919fe256e27dac440c6527
Colin <colincas37@gmail.com>
2023-01-12 12:49:43 -0800

    added mongodb and sdk

--- app.js
+++ app.js
@@ -2,10 +2,14 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send("Hello Bekonix Team!");
+    res.send("Hello Bekonix Team and Friends!");
 });
 
-const port = process.env.port || 3000;
+app.get('/timelapse-device', (req, res) => {
+    res.send("This is where the timelapse camera will accept data")
+});
+
+const port = process.env.port || 3000; //port variable holds preferred by environment (to make aws happy) or port 3000 to run locally 
 
 app.listen(port, () => {
     console.log("IM ALLLIVVE")
